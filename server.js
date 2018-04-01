7888deaa48ef3297561f633722d54429a2393b8d
Youplaf <37189944+Youplaf@users.noreply.github.com>
2018-04-01 13:03:33 +0200

    /signin

--- server.js
+++ server.js
@@ -5,6 +5,29 @@ const app = express();
 
 app.use(bodyParser.json());
 
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
+
 app.get('/', (req, res) => {
 
 });
@@ -14,7 +37,11 @@ app.get('/profile/:userId', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-
+	if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+		res.json('success');
+	} else {
+		res.status(400).json('error logging in');
+	}
 });
 
 app.post('/register', (req, res) => {
