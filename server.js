b20bc5897f404ecda0a7c092f57ca4ec7c9ec348
Youplaf <37189944+Youplaf@users.noreply.github.com>
2018-04-01 14:30:00 +0200

    /profile/:id and /image

--- server.js
+++ server.js
@@ -29,11 +29,21 @@ const database = {
 
 
 app.get('/', (req, res) => {
-
+	res.json(database.users);
 });
 
-app.get('/profile/:userId', (req, res) => {
-
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		}
+	});
+	if(!found) {
+		res.status(400).json('no such user');
+	}
 });
 
 app.post('/signin', (req, res) => {
@@ -45,11 +55,31 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	});
+	res.json(database.users[database.users.length-1]);
 });
 
 app.put('/image', (req, res) => {
-
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		}
+	});
+	if(!found) {
+		res.status(400).json('no such user');
+	}	
 });
 
 app.listen(3000, () => {
