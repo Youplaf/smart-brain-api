4ed76dca5420c2ebd4011a21d9dd7a0ceea0e393
Youplaf <37189944+Youplaf@users.noreply.github.com>
2018-04-04 19:49:57 +0200

    security review

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,22 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'f47e539c4a5c4b4abcc8a58292adc52e'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        req.body.input
+      )
+      .then(data => {
+      	res.json(data);
+      })
+      .catch(err => res.status(400).json('unable to work with API'));
+}
+
+
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
 
@@ -12,5 +31,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
+	handleApiCall: handleApiCall,
 	handleImage: handleImage
 }
