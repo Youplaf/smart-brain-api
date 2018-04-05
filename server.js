514e84657272c5d1a6f6e7a5fa6601178225592f
Youplaf <37189944+Youplaf@users.noreply.github.com>
2018-04-05 19:42:30 +0200

    changing get /

--- server.js
+++ server.js
@@ -24,9 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.json(database.users);
-});
+app.get('/', (req, res) => { res.send('it is working!') });
 
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
 
